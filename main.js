6dda6a8011eac9fd24da4a5fd72b4830d5a49d2f
bcheney21 <brinn_cheney@icloud.comgit config --global user.email brinn_cheney@icloud.com>
2021-02-03 15:53:55 -0800

    buggy but code along

--- main.js
+++ main.js
@@ -1,28 +1,50 @@
-const addPerson = (person)=>{
-  // create the li that will contain all this person's info
-  let li = document.createElement("li");
+class Person {
+  constructor(person) {
+    this.firstName = person.name.first
+    this.lastName = person.name.last
+    this.title = person.name.title
+    this.email = person.email
+    this.cell = person.cell
+    this.photo = person.picture.thumbnail
+  }
+ 
+  fullName() {
+    return this.title + this.firstName + this.lastName
+  }
 
-  // the h2 that contains their name is one part
-  const nameH2 = document.createElement('h2')
-  nameH2.innerText = `${person.name.title} ${person.name.first} ${person.name.last}`
+  contactInfo(){
+    return 'Email: ' + this.email + ', Cell: ' + this.cell
+  }
 
-  // the p that contains their contact info is next
-  const contactParagraph = document.createElement('p')
-  contactParagraph.innerText = `Email: ${person.email}, Cell: ${person.cell}`
+  generateNameH2(){
+    const nameH2 = document.getElementById('h2')
+    nameH2.innerText = this.fullName()
+    return nameH2
+  }
 
-  // image is last
-  const picture = document.createElement('img')
-  picture.src = person.picture.thumbnail
+  addToDom() {
+    let li = document.createElement("li");
+
+    // the h2 that contains their name is one part
+    const nameH2 = this.generateNameH2()
+
+    // the p that contains their contact info is next
+    const contactParagraph = document.createElement('p')
+    contactParagraph.innerText = this.contactInfo()
   
-  // put all 3 components into the li
-  li.append(nameH2)
-  li.append(contactParagraph)
-  li.append(picture)
+    // image is last
+    const picture = document.createElement('img')
+    picture.src = this.photo
     
-  // put the li into the list
-  document.querySelector('#people-list').appendChild(li);
+    // put all 3 components into the li
+    li.append(nameH2)
+    li.append(contactParagraph)
+    li.append(picture)
+      
+    // put the li into the list
+    document.querySelector('#people-list').appendChild(li);
+  }
 }
-
 
 document.addEventListener("DOMContentLoaded", ()=>{
   const requestUrl = "https://randomuser.me/api/?results=10";
@@ -37,7 +59,10 @@ document.addEventListener("DOMContentLoaded", ()=>{
 
       // put the people into the list
       let people = jsonData.results;
-      people.forEach(p => addPerson(p));
+      let babyStepsPerson = people[0]
+      let personInstance = new Person(babyStepsPerson)
+      debugger
+      // people.forEach(p => addPerson(p));
   })
   .catch((error)=>{
       // If any error is sent bac, you will have access to it here.
